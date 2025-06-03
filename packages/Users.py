00f04bcb850f180aca49e71364f7d764dd0c1425
Alexis Valerio <alexis.valerio83@gmail.com>
2025-06-03 09:56:12 -0400

    Actualizando repositorio, para continuar con proyecto

--- packages/Users.py
+++ packages/Users.py
@@ -1,8 +1,17 @@
 from pydantic import BaseModel, field_validator
-from typing import List
+from typing import List,Dict
 import logging #importar libreria para loging
-from dotenv import load_dotenv
-import os
+#libreria para la insercion de datos
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate('credenciales.json')
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+#from dotenv import load_dotenv
+#import os
 #configuracion del log, 
 logging.basicConfig(level=10, format='%(asctime)s %(levelname)s: %(message)s')
 
@@ -10,48 +19,52 @@ logging.basicConfig(level=10, format='%(asctime)s %(levelname)s: %(message)s')
 #load_dotenv()
 #variable = os.getenv("ALEXIS")
 
-usuario1 = {'rut_usuario':'15548355-5',
-            'nombre':'Alexis',
-            'apellido':'Valerio',
-            'nom_user':'avalerio',
-            'passw':'123',
-            'email':'asdas@asd.com'
+usuario1 = {'rut_usuario':'155483455-5',
+            'datos_user':[{
+                                'nombre':'Alexis',
+                                'apellido':'Valerio',
+                                'nom_user':'avalerio',
+                                'passw':'123',
+                                'email':'asdas@asd.com'
+                            }]
             }
 
-#UsuariosBD = ['avalerio','bvalerio','svec']
 
 #CREACION DE LA CLASE USUARIO 
-class Users(BaseModel):
-    rut_usuario:str
+class datos_usuario(BaseModel):
     nombre:str
     apellido:str
     nom_user:str
     passw:str
     email:str
+    
 
-    @field_validator('nom_user')
-    def validar_nom_usuario(cls,v):
-        UsuariosBD = ['avalerio','bvalerio','svec']
-        logging.info(f'usuario:{v}')
-        if v in UsuariosBD:
-            logging.info('Usuario ya existe')
-            raise ValueError('Usuario ya existe')
-        else:
-            logging.info('Generando nuevo usuario')
-            
+class Users(BaseModel):
+    rut_usuario:str
+    datos_user:list[datos_usuario]
+           
+
+    def guardar_usuario(data:dict):
+        data = data
+        
+        print(data)
+        logging.info("Creando Usuario")
+        indice = data['rut_usuario']
+        logging.info(f"Guardando el indice {indice}")
+        #print(data['datos_user'][0])
+        data['datos_user'] = dict(data['datos_user'][0])
+        print(data)
+
+        resultado = db.collection("Usuarios").document(indice)
+        resultado.set(data)
+        return indice
 
 user1 = Users(**usuario1)
-print(user1)
+#print(user1)
+#print(dict(user1))
+data = dict(user1)
 
-    
-        
-    
-
-
-
-logging.info("creando un usuario")
-crear = Users(**usuario1)
-logging.info("Usuario creado")
-print(crear.nom_user)
+salida = Users.guardar_usuario(data)
+print(salida)
 
 
